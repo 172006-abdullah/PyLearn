ae2048dd1f0294ad8dbabfa64bc9f1f61e3e3326
172006-abdullah <MUHAMMADABDULLAH172006@gmail.com>
2025-09-11 20:57:13 +0500

    star patterns program

---
+++ chapter 7/starpattern.py
@@ -0,0 +1,6 @@
+no= int(input("enter a number: "))
+
+for i in range(no):
+    for j in range(no): # rows
+        print("*",end=" ") # columns
+    print()
--- chapter 8/proj1.py
+++ chapter 8/proj1.py
@@ -1,7 +1,7 @@
-import random 
+import random
 '''
-1 for snake 
--1 for water 
+1 for snake
+-1 for water
 0 for gun
 '''
 
